546f0e53b854451d6a5f7d47c676e51030f34ee4
HenryTurbedsky <turbo.henryg@gmail.com>
2021-07-22 17:40:39 +0000

    added failedAttempts

--- public/js/viewMessages.js
+++ public/js/viewMessages.js
@@ -1,18 +1,28 @@
 
 
 
+let failedAttempts = 0;
+
 const getMessages = () => {
+
     const passcode = document.querySelector("#passcode")
     const messagesRef = firebase.database().ref();
+    const message = document.querySelector("#message")
 
     messagesRef.on('value', (snapshot) => {
+        failedAttempts++;
+        
         const data = snapshot.val()
         for(let key in data)
         {
             if(key === passcode.value)
             {
-                const message = document.querySelector("#message")
                 message.innerHTML = data[key]
+                failedAttempts = 0
+            }
+            else
+            {
+                message.innerHTML = "You have failed: " + failedAttempts
             }
         }
     })
